e3bf5d42f3001345b821be23dfdec220012dfe66
Sergey <68073864+seregatheone@users.noreply.github.com>
2020-07-26 00:22:53 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-#Token = '1320756189:AAFxRCswZg0Qffav1VPw4WwAgCVgBdoUiaw'
+Token = ***
 # welcoming_msg = "Howdy, partner! I'll service you as the encrypter/decrypter.\nKey for encryption is the same for decryption; they're generating randomly every single time.\nIf you want me to encrypt something, send me 'encrypt', if you want me to decrypt, send me 'decrypt'. "
-Token = "1291055299:AAHppDsJi_FfJGA4mYHZtoBo6MQjQ-jn_5A"
+
